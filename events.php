d0c5a2daecf3fc5c1fa7c68a5fb64e4b5207a0c6
Jeremy Mazzocco <68953007+jeremy-mazzocco@users.noreply.github.com>
2023-10-02 18:17:27 +0200

    login done

---
+++ events.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eventi</title>
+</head>
+
+<body>
+    <div>
+        <?php
+        include 'db_connection.php';
+        ?>
+        <h2>
+            qui gli eventi
+        </h2>
+    </div>
+
+
+</body>
+
+</html>
--- sql_login.php
+++ sql_login.php
@@ -1,10 +1,24 @@
 <?php
-    include 'db_connection.php';
 
-    // Registration
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        
-        
+include 'db_connection.php';
+
+// Login
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
+
+        $email = $_REQUEST['email'];
+        $password = $_REQUEST['password'];
+
+        $sql = "SELECT * FROM utenti WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
+        $user = mysqli_fetch_assoc($result);
+
+        if (password_verify($password, $user['password'])) {
+
+            header('Location: events.php');
+            exit;
+        }
     }
-    mysqli_close($conn);
-
+}
+mysqli_close($conn);
